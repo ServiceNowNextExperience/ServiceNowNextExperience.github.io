a5993da8cd94da4bad80a44eb1169d111a085c30
dale.stubblefield <dale.stubblefield@servicenow.com>
2024-04-17 13:23:08 -0500

    Updated tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'ServiceNow Next Experience',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Ready to learn something new?',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
